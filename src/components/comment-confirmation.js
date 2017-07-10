65bac3f442227d62afe45297bd569e96aa4edef9
Luisangonzalez <luisangonzalez@gmail.com>
2017-07-11 00:28:03 +0200

    first commit, refactor to use react create app

---
+++ src/components/comment-confirmation.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class CommentConfirmation extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showConfirm: false
+    };
+  }
+
+  render() {
+
+    let confirmNode;
+
+    if (this.state.showConfirm) {
+      return (
+        <span>
+          <a href="" onClick={this._confirmDelete.bind(this)}>Yes </a> - or - <a href="" onClick={this._toggleConfirmMessage.bind(this)}> No</a>
+        </span>
+      );
+    } else {
+      confirmNode = <a href="" onClick={this._toggleConfirmMessage.bind(this)}>{this.props.children}</a>;
+    }
+
+    return (
+      <span>
+        {confirmNode}
+      </span>
+    );
+  }
+
+  _toggleConfirmMessage(e) {
+    e.preventDefault();
+
+    this.setState({
+      showConfirm: !this.state.showConfirm
+    });
+
+  }
+
+  _confirmDelete(e) {
+    e.preventDefault();
+    this.props.onConfirm();
+  }
+}
+
+CommentConfirmation.propTypes = {
+  onConfirm: PropTypes.func.isRequired
+};
